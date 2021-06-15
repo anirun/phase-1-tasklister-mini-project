e732669898f1e1557a90ce4e92dbe8f8950d76bc
Nurina <nurina.junus@gmail.com>
2021-06-14 21:02:10 -0600

    completed challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  // grab task form
+  const createTaskForm = document.getElementById("create-task-form");
+
+  // grab task list with id "tasks"
+  const taskList = document.getElementById("tasks");
+
+  // attach event listener to form submission
+  createTaskForm.addEventListener("submit", (e) => {
+    
+    // prevent default action of redirecting to new page upon submit
+    e.preventDefault();
+    
+    // capture new task input
+    const newTask = document.getElementById("new-task-description").value;
+    
+    // add newTask HTML string to existing taskList HTML
+    taskList.innerHTML += `<li> ${newTask} </li>`;
+    
+    // reset form upon submission
+    createTaskForm.reset();
+  });
+  
 });
